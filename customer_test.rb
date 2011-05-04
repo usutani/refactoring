da279100a9c6bfe47da0e0748b2d124bc4509657
Yasuhiro Usutani <y_usutani@me.com>
2011-05-04 23:34:24 +0900

    1.8 ついに継承の導入か

--- customer_test.rb
+++ customer_test.rb
@@ -6,15 +6,15 @@ class TestCustomer < Test::Unit::TestCase
   def test_regular_and_new_release
     c = Customer.new("Usutani")
     assert_equal(c.statement, "Rental Record for Usutani\nAmount owed is 0\nYou earned 0 frequent renter points")
-    c.add_rental(Rental.new(Movie.new("Star Wars Episode IV", Movie::REGULAR), 3))
+    c.add_rental(Rental.new(Movie.new("Star Wars Episode IV", RegularPrice.new), 3))
     assert_equal(c.statement, "Rental Record for Usutani\n\tStar Wars Episode IV\t3.5\nAmount owed is 3.5\nYou earned 1 frequent renter points")
-    c.add_rental(Rental.new(Movie.new("Page One", Movie::NEW_RELEASE), 3))
+    c.add_rental(Rental.new(Movie.new("Page One", NewReleasePrice.new), 3))
     assert_equal(c.statement, "Rental Record for Usutani\n\tStar Wars Episode IV\t3.5\n\tPage One\t9\nAmount owed is 12.5\nYou earned 3 frequent renter points")
   end
   
   def test_childrens
     c = Customer.new("Usutani")
-    c.add_rental(Rental.new(Movie.new("UP!", Movie::CHILDRENS), 3))
+    c.add_rental(Rental.new(Movie.new("UP!", ChildrensPrice.new), 3))
     assert_equal(c.statement, "Rental Record for Usutani\n\tUP!\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points")
   end
 
--- movie_test.rb
+++ movie_test.rb
@@ -4,12 +4,8 @@ require "movie"
 
 class TestMovie < Test::Unit::TestCase
   def test_creation
-    m = Movie.new("Star Wars Episode IV", Movie::REGULAR)
+    m = Movie.new("Star Wars Episode IV", RegularPrice.new)
     assert_equal(m.title, "Star Wars Episode IV")
-    assert_equal(m.price_code, Movie::REGULAR)
-    
-    Movie::REGULAR
-    Movie::NEW_RELEASE
-    Movie::CHILDRENS
+    assert_equal(m.frequent_renter_points(1), 1)
   end
 end
